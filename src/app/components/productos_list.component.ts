d71f193cd75c98d961de2d317757c802c9b822b6
U-VAIO\Daniel <daniel_sephirot@hotmail.com>
2017-08-02 19:37:22 +0200

    create services,modules and select products

--- src/app/components/productos_list.component.ts
+++ src/app/components/productos_list.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
+import { ProductoService } from '../services/producto.service';
+import { Producto } from '../models/producto';
 
 @Component({
     selector: 'productos_list',
     templateUrl: '../views/productos_list.component.html',
-    styleUrls: ['../styles/productos_list.component.css']
+    styleUrls: ['../styles/productos_list.component.css'],
+    providers: [ProductoService]
 })
 
 export class ProductosListComponent {
     public title: string;
+    public productos: Array<Producto>;
 
     constructor(
         private _route: ActivatedRoute,
-        private _router: Router
+        private _router: Router,
+        private _productoService: ProductoService
     ) {
         this.title = "Listado de productos";
+        this.productos = [];
     }
 
     ngOnInit() {
         console.log("Se ha cargado el componente  productos_list.component.ts");
+        this._productoService.getProductos().subscribe(
+            result => {
+                if (result.code != 200) {
+                    console.log(result);
+                } else {
+                    this.productos = result.data;
+                    console.log(this.productos);
+                }
+            },
+            error => {
+                let errorMessage = <any>error;
+                console.log(errorMessage);
+            }
+        );
     }
 }
