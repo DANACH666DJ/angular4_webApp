2f6ea506d07437ef272d51335c51fa40a1c4a7e8
U-VAIO\Daniel <daniel_sephirot@hotmail.com>
2017-08-04 14:47:50 +0200

    add images products ok

--- src/app/components/producto_add.component.ts
+++ src/app/components/producto_add.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ProductoService } from '../services/producto.service';
 import { Producto } from '../models/producto';
 import { NgForm } from "@angular/forms/src/forms";
+import { GLOBAL } from '../services/global';
 
 @Component({
     selector: 'producto_add',
@@ -14,6 +15,8 @@ import { NgForm } from "@angular/forms/src/forms";
 export class ProductoAddComponent {
     public title: string;
     public producto: Producto;
+    public filesToUpload;
+    public resultUpload;
 
     constructor(
         private _route: ActivatedRoute,
@@ -30,10 +33,30 @@ export class ProductoAddComponent {
     }
 
     onSubmit(f: NgForm) {
+        //si hay imagen, guardamos el producto con la imagen
+        if (this.filesToUpload && this.filesToUpload.length >= 1) {
+            this._productoService.makeFileRequest(GLOBAL.url + 'upload-file', [], this.filesToUpload)
+                .then((result) => {
+                    console.log(result);
+                    this.resultUpload = result;
+                    this.producto.imagen = this.resultUpload.filename;
+                    this.saveProduct();
+                }, (error) => {
+                    console.log(error);
+                });
+            //si no hay imagen creamos el producto sin la imagen
+        } else {
+            this.saveProduct();
+        }
+
+        //f.resetForm();
+    }
+
+    saveProduct() {
         this._productoService.addProducto(this.producto).subscribe(
             response => {
-                if(response.code == 200){
-                    this._router.navigate(['/home']);
+                if (response.code == 200) {
+                    this._router.navigate(['/productos_list']);
                 } else {
                     console.log(response);
                 }
@@ -42,7 +65,13 @@ export class ProductoAddComponent {
                 console.log(<any>error);
             }
         );
-        f.resetForm();
+
+    }
+
+    fileChangeEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+
     }
 
 
